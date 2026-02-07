c7742fa7cd39a7b977803575d4cf5d6ae0221f25
Jignesh <jigneshkushwaha63@gmail.com>
2026-02-07 11:15:02 +0530

    added dynamic year in footer

--- js/resp.js
+++ js/resp.js
@@ -1,10 +1,13 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navList = document.querySelector('.nav-list')
-rightNav = document.querySelector('.right-nav')
+burger = document.querySelector(".burger");
+navbar = document.querySelector(".navbar");
+navList = document.querySelector(".nav-list");
+rightNav = document.querySelector(".right-nav");
+year = document.querySelector("#year");
 
-burger.addEventListener('click',()=>{
-navbar.classList.toggle('h-nav');
-navList.classList.toggle('v-class');
-rightNav.classList.toggle('v-class');
-})
+year.textContent = new Date().getFullYear();
+
+burger.addEventListener("click", () => {
+  navbar.classList.toggle("h-nav");
+  navList.classList.toggle("v-class");
+  rightNav.classList.toggle("v-class");
+});
